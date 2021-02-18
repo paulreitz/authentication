a4233ddd1778bb945e8a1003cfb68d02a18d4656
paulreitz <avenose@gmail.com>
2021-02-18 07:54:01 -0800

    Added accounts table

--- sql/database20210217.sql
+++ sql/database20210217.sql
@@ -1,2 +1,21 @@
+----------------- EXTENSIONS ------------------
 CREATE EXTENSION pgcrypto;
 CREATE EXTENSION "uuid-ossp";
+
+----------------- TABLES ------------------
+--ACCOUNTS
+CREATE TABLE accounts (
+    id serial PRIMARY KEY,
+    user_name varchar(256) unique,
+    name varchar(256),
+    password text,
+    created_at date
+);
+
+----------------- TYPES ------------------
+CREATE TYPE failure_action AS (
+    success boolean,
+    message text
+);
+
+----------------- FUNCTIONS ------------------
