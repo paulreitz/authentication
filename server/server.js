945815fd8d2c7c0c84f4ced6d1575f6c41098705
paulreitz <avenose@gmail.com>
2021-03-02 09:40:30 -0800

    Setup the basic server.

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-console.log('this is the server and stuff');
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const router = express.Router();
+const publicPath = path.join(__dirname, '..', 'build');
+const port = process.env.PORT || 3008;
+
+app.unsubscribe(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use('/api', router);
+app.use(express.static(publicPath));
+app.get('*', (__req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
