6927269964a2fdc8e8a764acd9335645a198bdab
paulreitz <avenose@gmail.com>
2021-03-02 09:52:50 -0800

    Created the base controller.

---
+++ server/controllers/BaseController.js
@@ -0,0 +1,24 @@
+class BaseController {
+    constructor(router, path) {
+        this.router = router;
+        this.path = path;
+    }
+
+    setPaths() {
+        throw new Error('BaseController.setPaths must be overridden by the child class');
+    }
+
+    endPointPath(endpoint) {
+        return `${this.path}/${endpoint}`;
+    }
+
+    makeGetPath(endpoint, callback) {
+        this.router.route(this.endPointPath(endpoint)).get(callback);
+    }
+
+    makePostPath(endpoint, callback) {
+        this.router.route(this.endPointPath(endpoint)).post(callback);
+    }
+}
+
+export default BaseController;
