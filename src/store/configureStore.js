c609e44f821664f8155dbf532de00b41df548e73
paulreitz <avenose@gmail.com>
2021-03-15 13:40:08 -0700

    Added the project reducer to the store configuration.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,13 +3,15 @@ import thunk from 'redux-thunk';
 
 import AccountReducer from './reducers/AccountReducer';
 import ProjectListReducer from './reducers/ProjectListReducer';
+import ProjectReducer from './reducers/ProjectReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     combineReducers({
         account: AccountReducer,
-        projects: ProjectListReducer
+        projects: ProjectListReducer,
+        project: ProjectReducer
     }),
     composeEnhancers(applyMiddleware(thunk))
 );
